f9b3532bb3280c2a8ef44d5a0c67f86aee032185
Phan Cong Hieu <phanconghieu12@gmail.com>
2024-07-28 00:55:43 +0700

    fix message page

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,24 +1,3 @@
-// import classNames from "classnames/bind";
-// import styles from "./DefaultLayout.module.scss";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-
-// const cx = classNames.bind(styles);
-
-// function DefaultLayout({ children }) {
-//     return (
-//         <div className={cx("wrapper")}>
-//             <Header />
-//             <div className={cx("container")}>
-//                 <div className={cx("content")}>{children}</div>
-//             </div>
-//             <Footer />
-//         </div>
-//     );
-// }
-
-// export default DefaultLayout;
-
 import classNames from "classnames/bind";
 import styles from "./DefaultLayout.module.scss";
 import Header from "../components/Header";
@@ -29,11 +8,6 @@ const cx = classNames.bind(styles);
 const DefaultLayout = ({ children, isAuthenticated, setAuthenticated }) => {
     return (
         <div>
-            {/* <Header isAuthenticated={isAuthenticated} />
-            <div className="container">
-                <div className="content">{children}</div>
-            </div>
-            <Footer /> */}
             <div className={cx("wrapper")}>
                 <Header
                     isAuthenticated={isAuthenticated}
--- src/components/Layout/ProfileLayout/index.js
+++ src/components/Layout/ProfileLayout/index.js
@@ -1,10 +1,21 @@
+import classNames from "classnames/bind";
+import styles from "./Profile.module.scss";
 import Header from "../components/Header";
 
-function ProfileLayout({ children }) {
+const cx = classNames.bind(styles);
+
+function ProfileLayout({ children, isAuthenticated, setAuthenticated }) {
     return (
         <div>
-            <Header />
-            <div className="content">{children}</div>
+            <div className={cx("wrapper")}>
+                <Header
+                    isAuthenticated={isAuthenticated}
+                    setAuthenticated={setAuthenticated}
+                />
+                <div className={cx("container")}>
+                    <div className={cx("content")}>{children}</div>
+                </div>
+            </div>
         </div>
     );
 }
